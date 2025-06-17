849bec998745cdbd7c822f91a6f367fe1ba647ee
R.K.Subash <subashkohila24092007@gmail.com>
2025-06-17 14:51:21 +0530

    first commit in victus. move to about us

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const navLinksEl = document.querySelector(".nav .links")
 const navLinkEls = document.querySelectorAll(".nav .links .link")
 const cardsEl = document.querySelector("#testimonials .cards")
 const activeCardEl = document.querySelector("#testimonials .cards .card.active")
+const moverEls = document.querySelectorAll(
+	"#testimonials .cards-container .mover"
+)
 // End of elements refs
 
 const bgImage = new Image()
@@ -12,54 +15,87 @@ const BG_IMG_SRC = "./assets/background.jpg"
 bgImage.src = BG_IMG_SRC
 
 bgImage.onload = () => {
-  mainEl.style.backgroundImage = `url("${BG_IMG_SRC}")`
+	mainEl.style.backgroundImage = `url("${BG_IMG_SRC}")`
 
-  const observer = new IntersectionObserver(handleObserver)
-  document.querySelectorAll(".transition").forEach(el => observer.observe(el))
+	const observer = new IntersectionObserver(handleObserver)
+	document.querySelectorAll(".transition").forEach((el) => observer.observe(el))
 
-  function handleObserver(entries) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) return
-      entry.target.classList.toggle("intersecting", true)
-      observer.unobserve(entry.target)
-    })
-  }
+	function handleObserver(entries) {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting) return
+			entry.target.classList.toggle("intersecting", true)
+			observer.unobserve(entry.target)
+		})
+	}
 }
 
 setTimeout(
-  () =>
-    setInterval(() => {
-      const activeP_El = document.querySelector(
-        "main .content .text .questions p.visible"
-      )
-      let nextP_el
+	() =>
+		setInterval(() => {
+			const activeP_El = document.querySelector(
+				"main .content .text .questions p.visible"
+			)
+			let nextP_el
 
-      if (activeP_El.nextElementSibling == null)
-        nextP_el = document.querySelector(
-          "main .content .text .questions p:first-child"
-        )
-      else nextP_el = activeP_El.nextElementSibling
+			if (activeP_El.nextElementSibling == null)
+				nextP_el = document.querySelector(
+					"main .content .text .questions p:first-child"
+				)
+			else nextP_el = activeP_El.nextElementSibling
 
-      activeP_El.classList.toggle("visible", false)
-      nextP_el.classList.toggle("visible", true)
-    }, 3000),
-  1000
+			activeP_El.classList.toggle("visible", false)
+			nextP_el.classList.toggle("visible", true)
+		}, 3000),
+	1000
 )
 
 navTogglerEl.addEventListener("click", () => {
-  navLinksEl.classList.toggle("open")
-  navTogglerEl.classList.toggle("open")
+	navLinksEl.classList.toggle("open")
+	navTogglerEl.classList.toggle("open")
 })
 
-navLinkEls.forEach(el =>
-  el.addEventListener("click", () => {
-    navLinksEl.classList.toggle("open", false)
-    navTogglerEl.classList.toggle("open", false)
-  })
+navLinkEls.forEach((el) =>
+	el.addEventListener("click", () => {
+		navLinksEl.classList.toggle("open", false)
+		navTogglerEl.classList.toggle("open", false)
+	})
 )
 
 cardsEl.scrollBy({
-  behavior: "smooth",
-  left:
-    cardsEl.firstElementChild.clientWidth * parseInt(activeCardEl.dataset["i"]),
+	behavior: "smooth",
+	left:
+		cardsEl.firstElementChild.clientWidth * parseInt(activeCardEl.dataset["i"]),
 })
+
+function handleMoverClick({ currentTarget }) {
+	const disabledButton = document.querySelector(
+		"#testimonials .cards-container .mover:disabled"
+	)
+	if (disabledButton) disabledButton.disabled = false
+	let shouldDisable = false
+
+	const dir =
+		currentTarget.dataset.dir === "right"
+			? cardsEl.firstElementChild.clientWidth
+			: -cardsEl.firstElementChild.clientWidth
+	cardsEl.scrollBy({ left: dir })
+
+	const activeCardEl = document.querySelector(
+		"#testimonials .cards .card.active"
+	)
+	activeCardEl.classList.toggle("active", false)
+
+	if (dir > 0) {
+		activeCardEl.nextElementSibling.classList.toggle("active", true)
+		if (activeCardEl.nextElementSibling.nextElementSibling == null)
+			shouldDisable = true
+	} else {
+		activeCardEl.previousElementSibling.classList.toggle("active", true)
+		if (activeCardEl.previousElementSibling.previousElementSibling == null)
+			shouldDisable = true
+	}
+
+	currentTarget.disabled = shouldDisable
+}
+
+moverEls.forEach((el) => el.addEventListener("click", handleMoverClick))
